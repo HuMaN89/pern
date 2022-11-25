17bdec6e1674c2092a0d47f51304b857ad42dd5b
Kudabaev Bekzhan <Human_B_9@mail.ru>
2022-11-25 12:34:48 +0600

    feat: backend

--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -1,10 +1,12 @@
 const {Type} = require('../models/models')
 const ApiError = require('../error/ApiError')
+const { json } = require('sequelize')
 
 class TypeController {
   async create(req, res) {
     const {name} = req.body
-
+    const type = await Type.create({name})
+    return res.json(type)
   }
 
   async getAll(req,res) {
