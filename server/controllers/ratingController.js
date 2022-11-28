4189702a89987e35f2ea7cd37c016c3a4ed14a1d
HuMaN89 <Human_B_9@mail.ru>
2022-11-29 00:00:56 +0600

    feat: backend

---
+++ server/controllers/ratingController.js
@@ -0,0 +1,9 @@
+const {Rating} = require('../models/models')
+const ApiError = require('../error/ApiError')
+const {Device, Rating, User} = require('../models/models')
+
+class RatingCotroller {
+    async setRating(req, res) {
+        const {id } = User
+    }
+}
